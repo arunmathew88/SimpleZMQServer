af59657a59e78cc0c5cd57d6742285a118920c06
Arun Mathew <arunmathew88@gmail.com>
2014-04-09 21:20:33 +0530

    Mutex for counter updations and Some basic comments

--- src/SimpleZMQServer.c
+++ src/SimpleZMQServer.c
@@ -24,6 +24,7 @@
 #define SOCKET_PORT 4343
 
 long counter = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* ZMQServer(void *td) {
     void *context = zmq_ctx_new ();
@@ -40,10 +41,13 @@ void* ZMQServer(void *td) {
         	printf("ERROR: Error reading ZMQ message!\n");
         	continue;
         }
+        pthread_mutex_lock( &mutex );
         counter++;
+        pthread_mutex_unlock( &mutex );
+
         buffer[rc] = '\0';
         printf ("%s", buffer);
-        zmq_send (responder, "Acknowledged", 12, 0);
+        zmq_send (responder, "Acknowledged", 12, 0);	// Simple ZMQ REQ REP pattern requires strict alternation between send & receive.
     }
     return NULL;
 }
@@ -61,12 +65,15 @@ void* SocketServer(void *td) {
     serv_addr.sin_port = htons(SOCKET_PORT);
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     listen(listenfd, MAX_CONNECTIONS);
+
     while (1) {
 		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
 
+		// n+= .... keep appending to end of current buffer which might contain incomplete lines.
 		while((n += read(connfd, buffer + n, MAXLOGLENGTH - n)) > 0) {
 			int c = n;
 			buffer[n] = '\0';
+			//Find the last New Line character so that we can shift possible incomplete lines to beginning.
 			for ( ; c > 0; c--)
 				if (buffer[c] == '\n') {
 					last_nl = c;
@@ -74,15 +81,21 @@ void* SocketServer(void *td) {
 				}
 			line = strtok(buffer, "\n");
 			printf("%s\n", line);
-			counter++;
+	        pthread_mutex_lock( &mutex );
+	        counter++;
+	        pthread_mutex_unlock( &mutex );
+	        //Print all the complete lines from current buffer.
 			while ((line = strtok(NULL, "\n")) != NULL) {
 				if (line - buffer < last_nl) {
 					printf("%s\n", line);
-					counter++;
+			        pthread_mutex_lock( &mutex );
+			        counter++;
+			        pthread_mutex_unlock( &mutex );
 				} else {
 					break;
 				}
 			}
+			//For shifting the incomplete line to beginning of the buffer.
 			for (c=0, last_nl++; last_nl < n; c++, last_nl++) {
 				buffer[c] = buffer[last_nl];
 			}
@@ -94,13 +107,19 @@ void* SocketServer(void *td) {
 
 int main(void) {
 	pthread_t thread[2];
-//	pthread_create(&thread[0], NULL, ZMQServer, NULL);
+	pthread_create(&thread[0], NULL, ZMQServer, NULL);
 	pthread_create(&thread[1], NULL, SocketServer, NULL);
 	while (1) {
-		printf("Counter: %ld\n", counter);
+		long old_counter;
 		sleep(10);
+        pthread_mutex_lock( &mutex );
+        old_counter = counter;			//Copying the value so that the lock is for minimal period.
+        counter = 0;
+        pthread_mutex_unlock( &mutex );
+        if (old_counter == 0)
+        	printf("No message received in the last 10 second\n");
 	}
 	pthread_join(thread[1], NULL);
-//	pthread_join(thread[0], NULL);
+	pthread_join(thread[0], NULL);
 	return EXIT_SUCCESS;
 }
