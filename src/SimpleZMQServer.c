5726f643bac66b5cf674d11113a7ed3a3ac0d47e
Arun Mathew <arunmathew88@gmail.com>
2014-04-09 15:19:59 +0530

    Initial Commit

---
+++ src/SimpleZMQServer.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : SimpleZMQServer.c
+ Author      : Arun Mathew
+ Version     :
+ Copyright   : 
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	return EXIT_SUCCESS;
+}
